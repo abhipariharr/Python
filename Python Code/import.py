f14de4050ca5ab5cf4b8934aebaa700762000310
Abhishek parihar <114382172+abhipariharr@users.noreply.github.com>
2025-09-02 14:32:32 +0530

    Add files via upload

---
+++ Python Code/import.py
@@ -0,0 +1,9 @@
+# from Package.maths import addition
+# print(addition(2,3))
+
+from Package.maths import *
+print(addition(2,3))
+print(subtraction(4,3))
+
+from Package.subpackage.multi import *
+print(multiply(2,3))
